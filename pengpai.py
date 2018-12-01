ea8e344950f98905eefad42f635a5b3e7105789d
Hcyand <897488242@qq.com>
2018-12-01 18:14:57 +0800

    澎湃新闻刷新爬取到文本文件

--- pengpai.py
+++ pengpai.py
@@ -1,12 +1,13 @@
 # 抓取澎湃新闻练习 -- 完成
 # 澎湃新闻有下拉更新，交互练习 -- 完成
+# 存储到文本文件当中
+# （MongoDB）可视化 MYSQL?
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from pyquery import PyQuery as pq
 import time
-
 
 browser = webdriver.Chrome()
 wait = WebDriverWait(browser, 10)
@@ -18,7 +19,7 @@ def index_page():
         browser.get(url)
         try:
             # 下拉进度条，新闻刷新。循环6次，等待时间6秒
-            for i in range(1, 6):
+            for i in range(1, 3):
                 browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
                 time.sleep(6)
         except Exception as ex:
@@ -33,13 +34,14 @@ def get_products():
     doc = pq(html)
     items = doc('#mainContent .news_li').items()
     for item in items:
-        product = {
-            '标题': item.find('h2>a').text(),
-            '内容': item.find('p').text(),
-            '发布者': item.find('.pdtt_trbs>a').text(),
-            '发布时间': item.find('span').text()
-        }
-        print(product)
+        title = item.find('h2>a').text()
+        news = item.find('p').text()
+        author = item.find('.pdtt_trbs>a').text()
+        times = item.find('span').text()
+        file = open('pengpai.txt', 'a', encoding='utf-8')
+        file.write('\n'.join([title, news, author, times]))
+        file.write('\n' + '=' * 50 + '\n')
+        file.close()
 
 
 def main():
